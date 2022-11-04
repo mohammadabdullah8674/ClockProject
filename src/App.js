f11d03bc37846a1eea3c6b272b721eb50b945b56
Mohd Abdullah <98703879+mohammadabdullah8674@users.noreply.github.com>
2022-11-04 15:32:56 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+import Footer from "./components/Footer";
+import AnalogClock from "./components/AnalogClock";
+import DigitalClock from "./components/DigitalClock";
+
+export default function App() {
+  return (
+    <div>
+      <DigitalClock />
+      <AnalogClock />
+      <Footer />
+    </div>
+  );
+}
